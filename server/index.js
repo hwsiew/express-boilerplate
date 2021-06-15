595b2eaae1ae585cf9633d16f8c9956891cdaed2
hwsiew <hw.siew.work@gmail.com>
2021-06-15 13:31:59 +0800

    Updated to connect mongodb at server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,42 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const mongoose = require('mongoose');
+
+const MONGO_USER = process.env.MONGO_USER || 'root';
+const MONGO_PSWD = process.env.MONGO_PASSWORD || 'root';
+const MONGO_HOST = process.env.MONGO_HOST || 'mongodb';
+const MONGO_PORT = process.env.MONGO_PORT || 27017;
+const MONGO_DB	 = process.env.MONGO_DATABASE || 'test';
+const EXPRESS_PORT = process.env.PORT || 3000;
+
+const app 	= express();
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
-  })
-  
+	res.send('Hello World!');
+});
 
-app.listen(PORT, () => {
-	console.log(`Server is now listening on ${PORT}`);
+
+var connectMongoDB = function(){
+	const mongooseUri = `mongodb://${MONGO_USER}:${MONGO_PSWD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+	mongoose
+		.connect(
+			mongooseUri, 
+			{
+				useNewUrlParser: true, 
+				useUnifiedTopology: true
+			}
+		)
+		.then(() => {
+			console.log('[mongodb] is ready!');
+			app.emit('ready'); // emit 'ready' event when database is connected
+		})
+		.catch((err) => {
+			console.log(err);
+			setTimeout(connectMongoDB, 5000); // retry if connection failed
+		});
+}
+connectMongoDB(); // connect to db
+
+// start listening at designated port when db is ready
+app.on('ready', () => {
+	app.listen(EXPRESS_PORT, () => console.info(`[express] listening on port ${EXPRESS_PORT}`));
 });
